6ccb2161c47e21b2d67b1414f7b0794eaaf62823
Hipsterisiili <rasmus.marttila@hotmail.com>
2019-03-26 20:11:44 +0200

    unicafen testit lisätty

--- laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
+++ laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
@@ -17,4 +17,37 @@ public class MaksukorttiTest {
     public void luotuKorttiOlemassa() {
         assertTrue(kortti!=null);      
     }
+    
+    @Test
+    public void kortinSaldoOnOikea() {
+        assertEquals(kortti.saldo(), 10);      
+    }
+    
+    @Test
+    public void rahanLataaminenKasvattaaSaldoaOikein() {
+        kortti.lataaRahaa(111);
+        assertEquals(kortti.saldo(), 121);      
+    }
+    
+    @Test
+    public void saldoVaheneeOikein() {
+        kortti.otaRahaa(4);
+        assertEquals(kortti.saldo(), 6);      
+    }
+    
+    @Test
+    public void saldoEiVäheneJosKoyha() {
+        kortti.otaRahaa(100);
+        assertEquals(kortti.saldo(), 10);      
+    }
+    
+    @Test
+    public void palauttaaTrueKunPitaakin() {
+        assertEquals(kortti.otaRahaa(2), true);      
+    }
+    
+    @Test
+    public void palauttaaFalseKunPitaakin() {
+        assertEquals(kortti.otaRahaa(20), false);      
+    }
 }
