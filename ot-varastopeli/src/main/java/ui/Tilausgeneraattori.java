dbdae8408505bb5453ba7dbadc584ed51de16277
Hipsterisiili <rasmus.marttila@hotmail.com>
2019-04-16 16:41:52 +0300

    Korjattu projektin nimi

--- ot-varastopeli/src/main/java/ui/Tilausgeneraattori.java
+++ ot-varastopeli/src/main/java/ui/Tilausgeneraattori.java
@@ -1,5 +1,6 @@
 package ui;
 
+import java.util.Random;
 import sovelluslogiikka.Varasto;
 import sovelluslogiikka.Tuote;
 import sovelluslogiikka.Tilaus;
@@ -7,6 +8,20 @@ import sovelluslogiikka.Tilaus;
 public class Tilausgeneraattori {
     
     private Varasto a;
+    private Random rand;
     
+    public Tilausgeneraattori(Varasto varasto){
+        this.a = varasto;
+        this.rand = new Random();
+    }
     
+    public Tilaus aja(){
+        int i = rand.nextInt(100);
+        if(i > 50){
+            Tilaus t = new Tilaus(a);
+            return t;
+        } else {
+            return null;
+        }
+    }
 }
